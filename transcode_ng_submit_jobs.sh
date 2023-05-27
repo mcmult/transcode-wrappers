f7086fd9e42e550d560bcba5c2c03483fb318f8e
Tim McMullan <mcmult@gmail.com>
2023-05-26 22:08:42 -0400

    Add dependency argument to fix_mkv_title job

--- transcode_ng_submit_jobs.sh
+++ transcode_ng_submit_jobs.sh
@@ -8,11 +8,20 @@ module load slurm ffmpeg-tc
 OLD_IFS="$IFS"
 IFS=$'\n'
 
+#fix_mkv_title dependency list
+FIXUP_DEPEND=""
+
 for INFILE in $(find "${RAW_LOC}" -type f \( -name *.raw* -a ! -path *exclude* \) | sort ); do
 	JOB_NAME=$(basename $INFILE | sed 's/\.raw//' | sed 's/\ /_/g')
-	sbatch --job-name="${JOB_NAME}" /home/mcmult/transcode-wrappers/transcode_ng_sbatch.sh ${INFILE}
+	SUBMIT=$(sbatch --job-name="${JOB_NAME}" /home/mcmult/transcode-wrappers/transcode_ng_sbatch.sh ${INFILE})
+	JOB_ID=$(echo "${SUBMIT##* }")
+	if [[ -z ${FIXUP_DEPEND} ]]; then
+		FIXUP_DEPEND="afterany:${JOB_ID}"
+	else
+		FIXUP_DEPEND="${FIXUP_DEPEND},afterany:${JOB_ID}"
+	fi
 done
 
 IFS="$OLD_IFS"
 
-sbatch --ntasks=1 --cpus-per-task=1 /home/mcmult/transcode-wrappers/fix_mkv_title.sh "${FINAL_LOC}"
+sbatch --ntasks=1 --cpus-per-task=1 --dependency=${FIXUP_DEPEND} /home/mcmult/transcode-wrappers/fix_mkv_title.sh "${FINAL_LOC}"
